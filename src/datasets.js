e17384d3e536c771313e5c17dfd5e6cab2bfa171
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2021-11-24 10:07:23 -0500

    Updates

---
+++ src/datasets.js
@@ -0,0 +1,23 @@
+export const DATASETS = [
+  //{
+  //  name: 'First goal of World Cup 2014',
+  //  file: 'goal.tsv',
+  //},
+  // {
+  //   file: 'goal1.tsv',
+  //   terms: {'world':2, 'cup':2, 'brazil':1}
+  // },
+  {
+    name: '#COVID19',
+    //file: 'metaverseSententree.tsv',
+//    file: 'COVID19.tsv',
+    file: 'COVID19Sententree3.tsv',
+
+    
+  },
+  {
+    name: 'eat24',
+    file: 'eat24Sententree.tsv',
+  }
+];
+
